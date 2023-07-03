f350956174d1b4357ad7c01f7a76510da836c677
MassivePYR <jeovah02@gmail.com>
2023-07-03 11:22:02 -0300

    Atualizando a importação para o grupo generico

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,10 +1,10 @@
-import java.util.Date;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Restaurante extends Comercio {
     private long codigoRestaurante;
 
-    public Restaurante(String cnpj, String razaoSocial, String nomeFantasia, String telefone, String email, Date horarioAtendimento, Endereco endereco, Cidade cidade, long codigoRestaurante) {
+    public Restaurante(String cnpj, String razaoSocial, String nomeFantasia, String telefone, String email,
+            Date horarioAtendimento, Endereco endereco, Cidade cidade, long codigoRestaurante) {
         super(cnpj, razaoSocial, nomeFantasia, telefone, email, horarioAtendimento, endereco, cidade);
         this.codigoRestaurante = codigoRestaurante;
     }
