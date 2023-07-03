4056ee10889e1cb9b2c0e3e2f15015920e6de3a7
MassivePYR <jeovah02@gmail.com>
2023-07-03 12:51:01 -0300

    Atualização de Cadastro e Main

---
+++ src/CadastroEntregador.java
@@ -0,0 +1,8 @@
+public class CadastroEntregador extends Cadastro {
+    Long id;
+    String cnh;
+
+    public static void cadastrar() {
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bem-vindo ao 'iFood'!'");
+        System.out.println("Já é cadastrado? (S/N)");
+        String resposta = input.nextLine();
+        if (resposta == "S" || resposta == "s") {
+            Login();
+            // verificar se o login e senha estão corretos
+            // se estiverem, logar
+            // se não, pedir para digitar novamente
+        } else {
+            Cadastro();
+            // cadastrar o usuário
+        }
+        input.close();
     }
+
+    public static void Login() {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Digite seu login: ");
+            String login = input.nextLine();
+            System.out.println("Digite sua senha: ");
+            String senha = input.nextLine();
+            if (VerificarLogin(login, senha)) {
+                // logar
+                input.close();
+            } else {
+                System.out.println("Login ou senha incorretos. Digite novamente.");
+            }
+        }
+    }
+
+    private static boolean VerificarLogin(String login, String senha) {
+        // verificar no banco de dados se o login e senha estão corretos
+        return false;
+    }
+
+    public static void Cadastro() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Cadastrar como: ");
+        System.out.println("1 - Empresa");
+        System.out.println("2 - Entregador");
+        System.out.println("3 - Cliente");
+        int resposta = input.nextInt();
+        switch (resposta) {
+            case 1:
+                CadastroEmpresa.cadastrar();
+                break;
+            case 2:
+                CadastroEntregador.cadastrar();
+                break;
+            case 3:
+                CadastroPessoa.cadastrar();
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+
+        input.close();
+    }
+
 }
