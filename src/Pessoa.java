fa3de12b13d062e92e40ee45fad8729fd4d2a175
MassivePYR <jeovah02@gmail.com>
2023-07-03 11:21:29 -0300

    Renomeando a importação para o grupo mais genérico

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,16 @@
 public class Pessoa {
+    String CPF;
+    String nome;
+    String telefone;
+    String email;
+    String endereco;
+
+    public Pessoa(String CPF, String nome, String telefone, String email, String endereco) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
 }
