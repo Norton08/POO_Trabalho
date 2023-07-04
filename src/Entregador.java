f2b5ad6ebe5d42a3eaf24d35db1d9728fbb778e1
MassivePYR <jeovah02@gmail.com>
2023-07-03 23:10:59 -0300

    autoimplement

--- src/Entregador.java
+++ src/Entregador.java
@@ -1,9 +1,11 @@
 public class Entregador extends Pessoa {
     Long id;
     String cnh;
+    static int contador = 0;
 
-    public Entregador(String CPF, String nome, String telefone, String email, String endereco, Long id, String cnh) {
+    public Entregador(String CPF, String nome, String telefone, String email, String endereco, String cnh) {
         super(CPF, nome, telefone, email, endereco);
+        this.id = (long) contador++;
     }
 
     public static void cadastrar() {
