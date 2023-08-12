ed9f75b13d79de48b6e1037d834912236bd29762
MassivePYR <jeovah02@gmail.com>
2023-08-12 16:38:35 -0300

    introdução da interface grafica via JOptionPane cadastro endereço

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,22 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        //resgate de dados previamente cadastrados no banco de dados
+
+
+
+
+
+
         JOptionPane.showMessageDialog(null, "Olá, seja bem vindo ao sistema de entregas!");
         //menu principal com os botões de cadastro, consulta e sair
-        String[] opcoes = {"Cadastrar", "Consultar", "Sair"};
+        String[] opcoes = {"Cadastrar", "Consultar","Editar", "Sair"};
         int opcao = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Menu Principal",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
         switch (opcao) {
             case 0:
                 //menu de cadastro com os botões de cliente, comercio, entregador e produto
-                String[] opcoesCadastro = {"Cliente", "Comercio", "Entregador", "Produto"};
+                String[] opcoesCadastro = {"Cliente", "Comercio", "Entregador", "Produto","Endereço","Voltar"};
                 int opcaoCadastro = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Menu de Cadastro",
                         JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoesCadastro, opcoesCadastro[0]);
                 switch (opcaoCadastro) {
@@ -43,12 +50,50 @@ public class Main {
                         break;
                     case 1:
                         //cadastrar comercio
+                        String cnpj = JOptionPane.showInputDialog("Digite o CNPJ do comercio");
+                        String razaoSocial = JOptionPane.showInputDialog("Digite a razão social do comercio");
+                        String nomeFantasia = JOptionPane.showInputDialog("Digite o nome fantasia do comercio");
+                        String telefoneComercio = JOptionPane.showInputDialog("Digite o telefone do comercio");
+                        String emailComercio = JOptionPane.showInputDialog("Digite o email do comercio");
+                        String ruaComercio = JOptionPane.showInputDialog("Digite a rua do comercio");
+                        String loteComercio = JOptionPane.showInputDialog("Digite o lote do comercio");
+                        String quadraComercio = JOptionPane.showInputDialog("Digite a quadra do comercio");
+                        long numeroComercio = Long.parseLong(JOptionPane.showInputDialog("Digite o número do endereço do comercio"));
+                        String complementoComercio = JOptionPane.showInputDialog("Digite o complemento do comercio");
+                        String cepComercio = JOptionPane.showInputDialog("Digite o CEP do comercio");
+                        String cidadeComercio = JOptionPane.showInputDialog("Digite a cidade do comercio");
+                        Endereco enderecoComercio = new Endereco(loteComercio, quadraComercio, numeroComercio, cepComercio, complementoComercio, ruaComercio, cidadeComercio);
+                        Comercio comercio = new Comercio(cnpj, razaoSocial, nomeFantasia, telefoneComercio, emailComercio, enderecoComercio);
+                        JOptionPane.showMessageDialog(null, comercio);
                         break;
                     case 2:
                         //cadastrar entregador
+                        String cpfEntregador = JOptionPane.showInputDialog("Digite o CPF do entregador");
+                        String nomeEntregador = JOptionPane.showInputDialog("Digite o nome do entregador");
+                        String telefoneEntregador = JOptionPane.showInputDialog("Digite o telefone do entregador");
+                        String emailEntregador = JOptionPane.showInputDialog("Digite o email do entregador");
+                        String ruaEntregador = JOptionPane.showInputDialog("Digite a rua do entregador");
+                        String loteEntregador = JOptionPane.showInputDialog("Digite o lote do entregador");
+                        String quadraEntregador = JOptionPane.showInputDialog("Digite a quadra do entregador");
+                        long numeroEntregador = Long.parseLong(JOptionPane.showInputDialog("Digite o número do endereço do entregador"));
                         break;
                     case 3:
                         //cadastrar produto
+                        break;
+                    case 4:
+                        //cadastrar endereço
+                        String ruaEndereco = JOptionPane.showInputDialog("Digite a rua do endereço");
+                        String loteEndereco = JOptionPane.showInputDialog("Digite o lote do endereço");
+                        String quadraEndereco = JOptionPane.showInputDialog("Digite a quadra do endereço");
+                        long numeroEndereco = Long.parseLong(JOptionPane.showInputDialog("Digite o número do endereço"));
+                        String complementoEndereco = JOptionPane.showInputDialog("Digite o complemento do endereço");
+                        String cepEndereco = JOptionPane.showInputDialog("Digite o CEP do endereço");
+                        String cidadeEndereco = JOptionPane.showInputDialog("Digite a cidade do endereço");
+                        Endereco endereco1 = new Endereco(loteEndereco, quadraEndereco, numeroEndereco, cepEndereco, complementoEndereco, ruaEndereco, cidadeEndereco);
+                        JOptionPane.showMessageDialog(null, endereco1);
+                        break;
+                    case 5:
+                        //voltar
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Opção inválida!");
@@ -57,7 +102,7 @@ public class Main {
                 break;
             case 1:
                 //menu de consulta com os botões de cliente, comercio, entregador, produto, pedido e entrega
-                String[] opcoesConsulta = {"Cliente", "Comercio", "Entregador", "Produto", "Pedido", "Entrega"};
+                String[] opcoesConsulta = {"Cliente", "Comercio", "Entregador", "Produto", "Pedido", "Entrega","Voltar"};
                 int opcaoConsulta = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Menu de Consulta",
                         JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoesConsulta, opcoesConsulta[0]);
                 switch (opcaoConsulta) {
@@ -79,12 +124,46 @@ public class Main {
                     case 5:
                         //consultar entrega
                         break;
+                    case 6:
+                        //voltar
+                        break;
                     default:
                         JOptionPane.showMessageDialog(null, "Opção inválida!");
                         break;
                 }
                 break;
             case 2:
+                //editar
+                JOptionPane.showMessageDialog(null, "Obrigado por utilizar nosso sistema!");
+                //menu de edição com os botões de cliente, comercio, entregador, produto, pedido e entrega
+                String[] opcoesEdicao = {"Cliente", "Comercio", "Entregador", "Produto", "Pedido", "Entrega"};
+                int opcaoEdicao = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Menu de Edição",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoesEdicao, opcoesEdicao[0]);
+                switch (opcaoEdicao) {
+                    case 0:
+                        //editar cliente
+                        break;
+                    case 1:
+                        //editar comercio
+                        break;
+                    case 2:
+                        //editar entregador
+                        break;
+                    case 3:
+                        //editar produto
+                        break;
+                    case 4:
+                        //editar pedido
+                        break;
+                    case 5:
+                        //editar entrega
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opção inválida!");
+                        break;
+                }
+                break;
+            case 3:
                 //sair
                 JOptionPane.showMessageDialog(null, "Obrigado por utilizar nosso sistema!");
                 break;
@@ -95,17 +174,15 @@ public class Main {
 
 
 
-        Endereco e1 = new Endereco("lt 21", "qd 89", 123, "11111-123", "muro azul", "Rua qualquer", "Goiânia-Go");
-        Endereco e2 = new Endereco("lt 12", "qd 98", 321, "12111-114", "muro purpura", "Rua qualquer 2", "Goiânia-Go");
-        Cliente cliente = new Cliente("111.111.111-1", "Qualquer", "9998887776", "teste@teste.com", e1);
-        Comercio comercio = new Comercio("879495000178", "Qualquer SA", "Qualquer LTDA", "998298859", "qq@gmail.com",
-                e2);
-        Entregador entregador = new Entregador("111.111.111-1", "Qualquer", "9918867876", "teste@teste.com", e1,
-                "654987132");
-        Produto produto = new Produto(20.00, "produto qualquer");
-        Pedido pedido = new Pedido("30 minutos", Status.EmPreparo, produto, comercio);
-        Entrega entrega = new Entrega(pedido, entregador, cliente);
-        System.out.println(entrega);
+//        Endereco e1 = new Endereco("lt 21", "qd 89", 123, "11111-123", "muro azul", "Rua qualquer", "Goiânia-Go");
+//        Endereco e2 = new Endereco("lt 12", "qd 98", 321, "12111-114", "muro purpura", "Rua qualquer 2", "Goiânia-Go");
+//        Cliente cliente = new Cliente("111.111.111-1", "Qualquer", "9998887776", "teste@teste.com", e1);
+//        Comercio comercio = new Comercio("879495000178", "Qualquer SA", "Qualquer LTDA", "998298859", "qq@gmail.com", e2);
+//        Entregador entregador = new Entregador("111.111.111-1", "Qualquer", "9918867876", "teste@teste.com", e1, "654987132");
+//        Produto produto = new Produto(20.00, "produto qualquer");
+//        Pedido pedido = new Pedido("30 minutos", Status.EmPreparo, produto, comercio);
+//        Entrega entrega = new Entrega(pedido, entregador, cliente);
+//        System.out.println(entrega);
 
     }
 }
